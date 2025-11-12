ac0d31c28b244668d6e5326074ed19c249b42423
pushhhkar <2003pushkarkumar@gmail.com>
2025-11-12 22:51:06 +0530

    fix: correct CSS import path

--- eventify-ai/src/main.jsx
+++ eventify-ai/src/main.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' // Makes sure this imports your new App.jsx
-import './styles/app.css'       // This is likely where your background is set
+import './styles/App.css'       // This is likely where your background is set
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
